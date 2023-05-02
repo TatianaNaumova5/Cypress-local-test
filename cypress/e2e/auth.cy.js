b05a454de79fb6ba1f7734afc31e8f409737cb37
Tatiana Naumova <tatiananaumova5@gmail.com>
2023-05-01 22:49:13 -0400

    add hook beforeEach

--- cypress/e2e/auth.cy.js
+++ cypress/e2e/auth.cy.js
@@ -1,7 +1,9 @@
 describe('Auth', ()=>{
-  it('Sign in with valid credentials', ()=>{
+  beforeEach(()=>{
     cy.visit('https://localcoding.us/user/login')
+  })
 
+  it('Sign in with valid credentials', ()=>{
     cy.get('#normal_login_email').type('tatiananaumova5@gmail.com')
     cy.get('#normal_login_password').type('Liev2020!')
     cy.get('.login-form-button').click()
@@ -9,13 +11,10 @@ describe('Auth', ()=>{
   })
 
    it('Check if the placeholder contain input Email', ()=>{
-     cy.visit('https://localcoding.us/user/login')
      cy.get('#normal_login_email').should('be.visible')
    })
 
   it('Sign in form validation', ()=>{
-    cy.visit('https://localcoding.us/user/login')
-
     cy.get('#normal_login_email').should('have.value','')
     cy.get('#normal_login_password').should('have.value','')
     cy.get('.login-form-button').should('be.disabled')
@@ -67,17 +66,12 @@ describe('Auth', ()=>{
 
   })
 
-
-
   it('Check if the placeholder contain input Password', ()=>{
-    cy.visit('https://localcoding.us/user/login')
     cy.get('#normal_login_password').should('be.visible')
   })
 
 
   it('Login with incorrect credentials has massage "Auth failed"', () => {
-    cy.visit('https://localcoding.us/user/login')
-
     cy.get('#normal_login_email').type('test@gmail.com')
     cy.get('#normal_login_password').type('123123')
     cy.get('.login-form-button').click()
@@ -87,8 +81,6 @@ describe('Auth', ()=>{
   })
 
   it('Log in with not a valid email', () => {
-    cy.visit('https://localcoding.us/user/login')
-
     cy.get('#normal_login_email').type('somaEmail')
     cy.get('#normal_login_password').type('Liev2020!')
     cy.get('.ant-form-item-explain-error')
@@ -96,16 +88,13 @@ describe('Auth', ()=>{
   })
 
   it('Email is "Required"', () => {
-    cy.visit('https://localcoding.us/user/login')
     cy.get('#normal_login_email').type('some-text').clear()
     cy.get('.ant-form-item-explain-error').should('have.text', 'Required')
   })
 
   it('Password is "Required"', () => {
-    cy.visit('https://localcoding.us/user/login')
     cy.get('#normal_login_email').type('tatiananaumova5@gmail.com')
     cy.get('#normal_login_password').type('123456').clear()
     cy.get('.ant-form-item-explain-error').should('have.text', 'Required')
   })
-
 })
